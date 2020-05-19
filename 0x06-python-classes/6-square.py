4deeda9a0ac7e50096f031f6ca058834bf4b57dc
tatiana orejuela zapata <tatianaorezap@gmail.com>
2020-05-19 23:29:22 +0000

    Fixing print method

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -11,8 +11,8 @@ class Square:
             size (int): length of a side of the square
             position (int, int): coordinates of the square
         """
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
