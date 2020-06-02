0f283b58de8697c43bca2abf80aebe8e82de5fa0
tatsOre <tatianaorezap@gmail.com>
2020-06-02 17:59:26 -0500

    Inheritance adding tasks

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module for is_kind_of_class method"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Returns True if the object is an instance of, or if the object is an
+    instance of a class that inherited from the specified class, or False"""
+    return isinstance(obj, a_class)
