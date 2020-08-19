3f168703530217327163cdb6b05fe3e128743fcb
tatsOre <tatianaorezap@gmail.com>
2020-08-19 09:13:33 -0500

    [Task 6]

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,13 +1,25 @@
 #!/usr/bin/python3
-""" Test function find_peak """
+"""Module that finds a peak in a list of unsorted integers."""
+
+
+def findPeak(array, left, right):
+    """Aux Method"""
+    if left == right:
+        return array[left]
+    mid = int((left + right) / 2)
+    if array[mid] < array[mid + 1]:
+        return findPeak(array, mid + 1, right)
+    return findPeak(array, left, mid)
 
 
 def find_peak(list_of_integers):
-    """Testing"""
-    if len(list_of_integers) == 0:
-        return None
-    peak = list_of_integers[0]
-    for n in list_of_integers:
-        if n > peak:
-            peak = n
-    return peak
+    """Finds a peak in a list of unsorted integers."""
+    if not list_of_integers:
+        return
+    if len(list_of_integers) == 1:
+        return list_of_integers[0]
+    if list_of_integers[1] <= list_of_integers[0]:
+        return list_of_integers[0]
+    if list_of_integers[-1] >= list_of_integers[-2]:
+        return list_of_integers[-1]
+    return findPeak(list_of_integers, 0, len(list_of_integers) - 1)
